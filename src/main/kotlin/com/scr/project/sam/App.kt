696789b1c82de1d00b07a283c4048e05bfc840dd
scroutchy <stef.crombez@gmail.com>
2025-01-25 21:06:54 +0100

    feat: Spring application initialization

---
+++ src/main/kotlin/com/scr/project/sam/App.kt
@@ -0,0 +1,11 @@
+package com.scr.project.sam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceActorManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceActorManagementApplication>(*args)
+}
