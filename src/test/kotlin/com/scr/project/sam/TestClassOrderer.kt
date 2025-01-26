5df717f650c4b0b03f3925766d0b27a7669a97ba
scroutchy <stef.crombez@gmail.com>
2025-01-26 22:35:07 +0100

    test: Ordering test classes

---
+++ src/test/kotlin/com/scr/project/sam/TestClassOrderer.kt
@@ -0,0 +1,21 @@
+package com.scr.project.sam
+
+import org.junit.jupiter.api.ClassDescriptor
+import org.junit.jupiter.api.ClassOrderer
+import org.junit.jupiter.api.ClassOrdererContext
+import org.springframework.boot.test.context.SpringBootTest
+
+class TestClassOrderer: ClassOrderer {
+
+    override fun orderClasses(context: ClassOrdererContext?) {
+        context?.classDescriptors?.sortBy { weight(it) }
+    }
+
+    private fun weight(descriptor: ClassDescriptor): Int {
+
+        return when {
+            descriptor.isAnnotated(SpringBootTest::class.java) -> 2
+            else -> 1
+        }
+    }
+}
