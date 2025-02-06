903a5fd2099db56d7110b7fd216b5e3bcac52040
scroutchy <stef.crombez@gmail.com>
2025-02-06 17:56:06 +0100

    test: Test profile for AppTests

--- src/test/kotlin/com/scr/project/sam/domains/actor/component/ActorRepositoryTest.kt
+++ src/test/kotlin/com/scr/project/sam/domains/actor/component/ActorRepositoryTest.kt
@@ -10,13 +10,11 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.testcontainers.junit.jupiter.Testcontainers
 import reactor.kotlin.test.test
 import java.time.LocalDate
 import java.util.Locale
 
 @SpringBootTest
-@Testcontainers
 internal class ActorRepositoryTest(
     @Autowired private val actorRepository: ActorRepository,
     @Autowired private val actorDao: ActorDao,
