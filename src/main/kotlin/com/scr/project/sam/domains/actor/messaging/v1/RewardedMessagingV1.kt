840079623717b72572929d62d545bbc3bd856bc5
scroutchy <stef.crombez@gmail.com>
2025-05-29 17:00:16 -0300

    feat: Enable Kafka configuration and add integration tests for actor resource without Kafka

--- src/main/kotlin/com/scr/project/sam/domains/actor/messaging/v1/RewardedMessagingV1.kt
+++ src/main/kotlin/com/scr/project/sam/domains/actor/messaging/v1/RewardedMessagingV1.kt
@@ -1,7 +1,7 @@
 package com.scr.project.sam.domains.actor.messaging.v1
 
 import com.scr.project.commons.cinema.outbox.model.entity.Outbox
-import com.scr.project.commons.cinema.outbox.service.OutboxService
+import com.scr.project.commons.cinema.outbox.service.IOutboxService
 import com.scr.project.sam.config.TopicProperties
 import com.scr.project.sam.domains.actor.mapper.toHexString
 import com.scr.project.sam.domains.actor.mapper.toRewardedKafka
@@ -14,7 +14,7 @@ import reactor.core.publisher.Mono
 
 @Service
 @EnableConfigurationProperties(TopicProperties::class)
-class RewardedMessagingV1(private val producer: OutboxService, private val topicProperties: TopicProperties) {
+class RewardedMessagingV1(private val producer: IOutboxService, private val topicProperties: TopicProperties) {
 
     private val logger = LoggerFactory.getLogger(RewardedMessagingV1::class.java)
 
@@ -27,8 +27,8 @@ class RewardedMessagingV1(private val producer: OutboxService, private val topic
                 topicProperties.actorCreationNotification,
             )
         ).thenReturn(actor)
-            .doOnSubscribe { logger.debug("Sending actor creation notification for actor ${actor.id}") }
-            .doOnSuccess { logger.info("Sent actor creation notification for actor ${actor.id}") }
-            .doOnError { logger.warn("Failed to send actor creation notification for actor ${actor.id}") }
+            .doOnSubscribe { logger.debug("Handling process of actor creation notification for actor with id ${actor.id}") }
+            .doOnSuccess { logger.info("Notification creation process if any was successfully handled for actor with id  ${actor.id}") }
+            .doOnError { logger.warn("Failed to handle the process of actor creation notification for actor with id ${actor.id}") }
     }
 }
--- src/main/kotlin/com/scr/project/sam/domains/kafka/config/KafkaReactiveConfiguration.kt
+++ src/main/kotlin/com/scr/project/sam/domains/kafka/config/KafkaReactiveConfiguration.kt
@@ -1,11 +1,14 @@
 package com.scr.project.sam.domains.kafka.config
 
+import com.scr.project.commons.cinema.outbox.config.Properties.SPRING_KAFKA_ENABLED
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.kafka.sender.KafkaSender
 import reactor.kafka.sender.SenderOptions
 
 @Configuration
+@ConditionalOnProperty(name = [SPRING_KAFKA_ENABLED], havingValue = "true", matchIfMissing = false)
 class KafkaReactiveConfiguration(
     private val kafkaAvroProducerProperties: Map<String, Any>,
 ) {
